44a67b6b446479b730adf158e2c91bb14bbb4785
ioneone <ionejunhong@gmail.com>
2021-01-13 12:17:49 -0800

    use email as mfa verification for mint

--- mint.py
+++ mint.py
@@ -5,6 +5,11 @@ import os
 MY_GMAIL_ADDRESS = os.getenv('MY_GMAIL_ADDRESS')
 MINT_PASSWORD = os.getenv('MINT_PASSWORD')
 
-mint = mintapi.Mint(MY_GMAIL_ADDRESS, MINT_PASSWORD)
+mint = mintapi.Mint(
+    email=MY_GMAIL_ADDRESS,
+    password=MINT_PASSWORD,
+    mfa_method='email'
+    headless=True
+)
 
 print(mint.get_accounts())
