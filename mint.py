e320c36c41281b5c2c467c22ebad1aef221e9aa4
ioneone <ionejunhong@gmail.com>
2021-01-13 12:43:01 -0800

    remove headless option

--- mint.py
+++ mint.py
@@ -3,13 +3,16 @@ import mintapi
 import os
 
 MY_GMAIL_ADDRESS = os.getenv('MY_GMAIL_ADDRESS')
+MY_GMAIL_PASSWORD = os.getenv('MY_GMAIL_PASSWORD')
 MINT_PASSWORD = os.getenv('MINT_PASSWORD')
 
 mint = mintapi.Mint(
     email=MY_GMAIL_ADDRESS,
     password=MINT_PASSWORD,
     mfa_method='email',
-    headless=True
+    imap_account=MY_GMAIL_ADDRESS,
+    imap_password=MY_GMAIL_PASSWORD,
+    imap_server='imap.gmail.com'
 )
 
 print(mint.get_accounts())
