5dff7b7736cbf359fca0fb4929c5c80ae1318e98
ioneone <ionejunhong@gmail.com>
2021-01-13 17:20:06 -0800

    only execute nerdwallet task every month

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from weather import Client as WeatherClient
 from nerdwallet import Client as NerdWalletClient
 from datetime import date
 
+# Daily Tasks
 weatherClient = WeatherClient()
 weatherClient.send_email_if_rain_today()
 
-nerdWalletClient = NerdWalletClient()
-nerdWalletClient.check_net_worth()
+# Monthly Tasks
+if date.today().day == 1:
+    nerdWalletClient = NerdWalletClient()
+    nerdWalletClient.check_net_worth()
