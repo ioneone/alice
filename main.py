92bc42534cde8ac85c09c8b47230d7913c6f7f77
ioneone <ionejunhong@gmail.com>
2021-01-31 15:45:26 -0800

    remove networth tracker

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 """
-Assume `python main.py` is executed every morning, 
+Assume `python main.py` is executed every morning. 
 """
 
 from weather import Client as WeatherClient
 from nerdwallet import Client as NerdWalletClient
 from gmail import Client as GmailClient
 from datetime import date
+from typing import Callable
 
 gmailClient = GmailClient()
+weatherClient = WeatherClient()
+nerdWalletClient = NerdWalletClient()
+
+
+def tick(name: str, task: Callable[[], None], condition: bool):
+    if not condition:
+        return
+
+    try:
+        task()
+    except:
+        gmailClient.send(
+            f"{name} Error", f"Hi Junhong, something went wrong when running the task '{name}'")
+
 
 # Daily Tasks
-try:
-    weatherClient = WeatherClient()
-    weatherClient.send_email_if_rain_today()
-except:
-    gmailClient.send("Weather Client Error",
-                     "Hi Junhong, something went wrong when running WeatherClient.")
-
-    # Monthly Tasks
-if date.today().day == 1:
-    try:
-        nerdWalletClient = NerdWalletClient()
-        nerdWalletClient.append_net_worth_entry()
-    except:
-        gmailClient.send("NerdWallet Client Error",
-                         "Hi Junhong, something went wrong when running NerdWalletClient.")
+tick("Check Rain Today", weatherClient.send_email_if_rain_today, True)
