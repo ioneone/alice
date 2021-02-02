1e69551a67c9a09da511bc7cfe60b498aaac4ac4
ioneone <ionejunhong@gmail.com>
2021-02-01 19:02:58 -0800

    remove EmailMessage

--- notification.py
+++ notification.py
@@ -22,14 +22,13 @@ class Client:
 
     def send(self, to_addrs: str, subject: str, body: str):
         """Sends a message to me as Alice."""
-        msg = EmailMessage()
-        msg['From'] = f"Alice 🤖 <{BOT_GMAIL_ADDRESS}>"
-        msg['To'] = to_addrs
-        msg['Subject'] = subject
-        msg.set_content(body)
+        header = f"From: Alice 🤖 <{BOT_GMAIL_ADDRESS}>\r\nTo: {to_addrs}\r\nSubject: {subject}"
+        msg = f"{header}\r\n\r\n{body}".encode('utf8')
 
         with smtplib.SMTP('smtp.gmail.com') as connection:
             connection.starttls()
             connection.login(user=BOT_GMAIL_ADDRESS,
                              password=BOT_GMAIL_PASSWORD)
-            connection.send_message(msg)
+            connection.sendmail(from_addr=BOT_GMAIL_ADDRESS,
+                                to_addrs=to_addrs,
+                                msg=msg)
